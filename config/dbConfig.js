985ed52ee8e08a197cc31c60888decfbbd42dcf8
Saloni Gupta <sagupta@gammastack.com>
2022-02-14 17:54:35 +0530

    new

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: 'ec2-100-24-247-137.compute-1.amazonaws.com',
   USER: 'qawssvxopvwtbi',
-  PASSWORD: 'postgres',
+  PASSWORD: 'c2a54f41521225d741baa3ab2462f3413caf7e6eb76daf3061e494a13ac9bccf',
   DB: 'd3sksq4m11jj7d',
   dialect: 'postgres',
   dialectOptions: {
