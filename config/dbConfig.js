84a4f44b417426d52f8cc69f4c91c91ffd9e5925
Saloni Gupta <sagupta@gammastack.com>
2022-02-14 19:17:29 +0530

    new

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -4,12 +4,6 @@ module.exports = {
   PASSWORD: 'c2a54f41521225d741baa3ab2462f3413caf7e6eb76daf3061e494a13ac9bccf',
   DB: 'd3sksq4m11jj7d',
   dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
   pool: {
     max: 5,
     min: 0,
--- services/addTutorialsService.js
+++ services/addTutorialsService.js
@@ -2,11 +2,17 @@ import db from '../models'
 const Tutorial = db.tutorials;
 
 const addTutorialService = async (tutorial) => {
+    try{
     await Tutorial.create({
         title: tutorial.title,
         description: tutorial.description,
     })
     return "Sucessfull"
+}
+catch(error){
+console.log(error.name)
+return "error";
+}
 }
 
 export default addTutorialService
